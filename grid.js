c4163c9799cd0c1f155af1025d1a0c9af72cf8b3
Ilya Furman <smashlong@gmail.com>
2011-06-01 20:56:52 +0300

    Now we have a grid with arbitary number of rows and cols

--- grid.js
+++ grid.js
@@ -1,2 +1,30 @@
 // draw grid
 
+var Grid = function(node, options) {
+  if (!node || node.nodeName !== 'TABLE') {
+    throw 'Please provide a valid table node in order to initialize Grid';
+  }
+
+  // add defaults
+  options = _.extend(options || {}, {
+    rows : 5,
+    cols : 5,
+    idle : 1000
+  });
+
+  var fragment = document.createDocumentFragment();
+
+  this.draw = function() {
+    for (var i = 0; i < options.rows; i++) {
+      var row = _.create('tr');
+      for (var k = 0; k < options.cols; k++) {
+        row.appendChild( _.create('td') );
+      }
+      fragment.appendChild(row);
+    }
+
+    node.appendChild(fragment)
+  }
+
+  this.draw()
+}
--- utils.js
+++ utils.js
@@ -1,2 +1,35 @@
 // some helpers and cross-browser fixes
 
+_ = {
+  listen : function(element, type, callback) {
+    if ('attachEvent' in element) {
+      // hacky, but we have no images,
+      // and i think it'll do for 3h no library task
+      if (type === 'DOMContentLoaded') {
+        type = 'load';
+      }
+      element.attachEvent('on' + type, callback);
+    } else {
+      // we don't want to use useCapture now
+      element.addEventListener(type, callback, false);
+    }
+  },
+  extend : function(source, ext) {
+    for (var key in ext) {
+      if (typeof source[key] === 'undefined' &&
+          typeof ext[key] !== 'undefined' ) {
+        source[key] = ext[key];
+      }
+    }
+    return source;
+  },
+  id : function(id) {
+    return document.getElementById(id);
+  },
+  create : function(tag) {
+    return document.createElement(tag);
+  },
+  rgb : function() {
+
+  }
+}
